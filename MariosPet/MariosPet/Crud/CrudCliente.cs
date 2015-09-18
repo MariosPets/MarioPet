d73ddc4b6dc4afda3a40ba4649cc8040494fb499
Tsunemoto <tsunemoto_akiko@hotmail.com>
2015-09-18 08:55:57 -0300

    Revert "abc"
    This reverts commit 8d0418be31a172308d6c2f6f358ff18964523a93.

--- MariosPet/MariosPet/Crud/CrudCliente.cs
+++ MariosPet/MariosPet/Crud/CrudCliente.cs
@@ -15,7 +15,6 @@ namespace MariosPet.Crud
             using (OdbcConnection conexao = ConexaoPadrao.criarConexao())
             {
 
-
                 string sql = "insert into CLIENTE (ID_CLIENTE, ID_PESSOA) values(?,?)";
                 OdbcCommand command = new OdbcCommand(sql, conexao);
 
