d23e090588ca651c1525365f0e95d22a5007f0e6
leonardofernando06 <leonardo.fernando@gmail.com>
2015-09-18 12:31:16 -0300

    oiu

---
+++ MariosPet/MariosPet/Classes/ConexaoPadrao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariosPet.Classes
+{
+    class ConexaoPadrao
+    {
+        public static OdbcConnection createConnection()
+        {
+            string driver = @"SQL Server";
+            string server = @".\MSERVERSQL";
+            string dataBase = @"SystemManagingHouseholdTasks";
+
+            StringBuilder stringConnection = new StringBuilder();
+
+            stringConnection.Append("driver=");
+            stringConnection.Append(driver);
+            stringConnection.Append(";server=");
+            stringConnection.Append(server);
+            stringConnection.Append(";database=");
+            stringConnection.Append(dataBase);
+
+            return new OdbcConnection(stringConnection.ToString());
+        }
+    }
+}
