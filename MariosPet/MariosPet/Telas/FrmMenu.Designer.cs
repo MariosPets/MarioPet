0fd00256ee6cf19cc442be143941f5eaa58fd899
oliviasrs <oliviasrsiori@gmail.com>
2015-09-18 11:21:24 -0300

    dgsd

--- MariosPet/MariosPet/Telas/FrmMenu.Designer.cs
+++ MariosPet/MariosPet/Telas/FrmMenu.Designer.cs
@@ -790,6 +790,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(201)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(1339, 718);
             this.Controls.Add(this.button2);
