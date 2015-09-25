3c0ee9f786888d174dbae2bd6ba3fff9d11c5a1e
Tsunemoto <tsunemoto_akiko@hotmail.com>
2015-09-25 11:54:37 -0300

    classeservico

--- MariosPet/MariosPet/Classes/Servico.cs
+++ MariosPet/MariosPet/Classes/Servico.cs
@@ -8,5 +8,11 @@ namespace MariosPet.Classes
 {
     class Servico
     {
+        private int id_servico { get; set; }
+        private int id_animal { get; set; }
+        private int id_funcionario { get; set; }
+        private string tipo_servico { get; set; }
+        private DateTime hora_entrada_servico { get; set; }
+        private DateTime hora_saida_servico { get; set; }
     }
 }
