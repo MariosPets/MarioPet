0e7c45d7ae7f54c39611f9efa3e47bf6235ef9d7
leonardofernando06 <leonardo.fernando@gmail.com>
2015-09-25 08:03:09 -0300

    Classe Agendamento

---
+++ MariosPet/MariosPet/Classes/Agendamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariosPet.Classes
+{
+    class Agendamento
+    {
+        private int id_atendimento { get; set; }
+        private int id_animal { get; set; }
+        private DateTime hora_agendamento { get; set; }
+        private int id_funcionario { get; set; }
+        private string atendido { get; set; }
+    }
+}
