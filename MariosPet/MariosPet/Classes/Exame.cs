be563f9fc3312bffc0fd6d92b80a1b632d9f7b45
Tsunemoto <tsunemoto_akiko@hotmail.com>
2015-09-25 10:00:36 -0300

    classexame

--- MariosPet/MariosPet/Classes/Exame.cs
+++ MariosPet/MariosPet/Classes/Exame.cs
@@ -8,5 +8,11 @@ namespace MariosPet.Classes
 {
     class Exame
     {
+        private int id_exame { get; set; }
+        private int id_animal { get; set; }
+        private DateTime data_exame { get; set; }
+        private string exame { get; set; }
+        private int id_veterinario { get; set; }
+        private int id_laboratorio { get; set; }
     }
 }
