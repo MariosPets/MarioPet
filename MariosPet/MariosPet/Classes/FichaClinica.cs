416a21075833474cf53dc9ad0ed8abde49d9e569
Tsunemoto <tsunemoto_akiko@hotmail.com>
2015-09-25 10:03:35 -0300

    classefichaclinica

--- MariosPet/MariosPet/Classes/FichaClinica.cs
+++ MariosPet/MariosPet/Classes/FichaClinica.cs
@@ -8,5 +8,9 @@ namespace MariosPet.Classes
 {
     class FichaClinica
     {
+        private int id_ficha_clinica { get; set; }
+        private int id_animal { get; set; }
+        private int id_veterinario { get; set; }
+        private DateTime data_ficha_clinica { get; set; }
     }
 }
