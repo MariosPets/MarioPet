142663200b3c5fa88d1c699405f4f5b25ce14a73
leonardofernando06 <leonardo.fernando@gmail.com>
2015-09-25 08:00:01 -0300

    Classe Animal

--- MariosPet/MariosPet/Classes/Animal.cs
+++ MariosPet/MariosPet/Classes/Animal.cs
@@ -8,5 +8,13 @@ namespace MariosPet.Classes
 {
     class Animal
     {
+        private int id_animal { get; set; }
+        private string nome_animal { get; set; }
+        private int id_cliente { get; set; }
+        private string raca_porte { get; set; }
+        private string sexo { get; set; }
+        private string pelagem_cor { get; set; }
+        private DateTime nascimento { get; set; }
+        private int id_veterinario { get; set; }
     }
 }
