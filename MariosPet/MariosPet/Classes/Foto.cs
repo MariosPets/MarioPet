a165fb81bbe60f7d5f86631004a0feb7b6ec609d
Tsunemoto <tsunemoto_akiko@hotmail.com>
2015-09-25 10:09:44 -0300

    classefoto

--- MariosPet/MariosPet/Classes/Foto.cs
+++ MariosPet/MariosPet/Classes/Foto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,8 @@ namespace MariosPet.Classes
 {
     class Foto
     {
+        private int id_foto { get; set; }
+        private int id_animal { get; set; }
+        private Image foto { get; set; }
     }
 }
