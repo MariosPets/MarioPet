b05f17dc4bd29a53be36ce3211b5d299501fc96c
Tsunemoto <tsunemoto_akiko@hotmail.com>
2015-09-25 11:46:35 -0300

    classepessoajuridica

--- MariosPet/MariosPet/Classes/PessoaJuridica.cs
+++ MariosPet/MariosPet/Classes/PessoaJuridica.cs
@@ -8,5 +8,9 @@ namespace MariosPet.Classes
 {
     class PessoaJuridica
     {
+        private int id_pessoa_juridica { get; set; }
+        private string denominacao_social { get; set; }
+        private string cnpj { get; set; }
+        private int id_endereco { get; set; }
     }
 }
