a98f0e7849a974e5f60f64ebc9495d505cc32c00
Dardanos <pliniosp@hotmail.com>
2015-09-30 11:22:43 -0300

    Atualização de classe

---
+++ MariosPet/MariosPet/Classes/Medicamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariosPet.Classes
+{
+    class Medicamento
+    {
+        private int id { get; set; }
+        private string descricao { get; set; }
+    }
+}
