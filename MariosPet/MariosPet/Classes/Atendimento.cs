a609c5702eddc3646578fd3f5f46eb0bd993e6ef
Dardanos <pliniosp@hotmail.com>
2015-09-30 11:19:21 -0300

    Atualização de classe

--- MariosPet/MariosPet/Classes/Atendimento.cs
+++ MariosPet/MariosPet/Classes/Atendimento.cs
@@ -8,10 +8,10 @@ namespace MariosPet.Classes
 {
     class Atendimento
     {
-        private int id_atendimento { get; set; }
-        private int id_animal { get; set; }
-        private int id_funcionario { get; set; }
-        private DateTime hora_entrada_atendimento { get; set; }
-        private DateTime hora_saida_atendimento { get; set; }
+        private int id { get; set; }
+        private int idAnimal { get; set; }
+        private int idFuncionario { get; set; }
+        private DateTime horaEntrada { get; set; }
+        private DateTime horaSaida { get; set; }
     }
 }
