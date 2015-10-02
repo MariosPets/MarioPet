7cce5f0420b2318ffaa19a1ceab154821f6b4586
Dardanos <pliniosp@hotmail.com>
2015-10-02 07:46:10 -0300

    Adição da classe Produto

---
+++ MariosPet/MariosPet/Classes/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MariosPet.Classes
+{
+    class Produto
+    {
+        private int id { get; set; }
+        private string descricao { get; set; }
+        private string setor { get; set; }
+        private int idFornecedor { get; set; }
+        private string fabricante { get; set; }
+        private DateTime vencimento { get; set; }
+        private int quantidade { get; set; }
+        private string unidade { get; set; }
+        private double valorCusto { get; set; }
+        private double valorVenda { get; set; }
+        private double lucro { get; set; }
+        private string tributacao { get; set; }
+        private int minimoEmEstoque { get; set; }
+        private int maximoEmEstoque { get; set; }
+        private string codigoBarras { get; set; }
+    }
+}
