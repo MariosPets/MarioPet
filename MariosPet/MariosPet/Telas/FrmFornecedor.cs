7b6f385373cf8ef051aa001dbf1f5aa322dc34b5
JESSICA FERREIRA <jessica.ferreira7@fatec.sp.gov.br>
2015-10-02 08:51:02 -0300

    fornecedor

--- MariosPet/MariosPet/Telas/FrmFornecedor.cs
+++ MariosPet/MariosPet/Telas/FrmFornecedor.cs
@@ -16,5 +16,10 @@ namespace MariosPet.Telas
         {
             InitializeComponent();
         }
+
+        private void lblEmail_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
