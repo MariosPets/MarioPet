d7066cbbfddab79c346e0c0b8e188b9cc4752775
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-10-08 19:40:00 -0300

    ClasseVacinaPublic

--- MariosPet/MariosPet/Classes/Vacina.cs
+++ MariosPet/MariosPet/Classes/Vacina.cs
@@ -8,9 +8,9 @@ namespace MariosPet.Classes
 {
     class Vacina
     {
-        private int id { get; set; }
-        private int idAnimal { get; set; }
-        private int idMedicamento { get; set; }
-        private DateTime data { get; set; }
+        public int id { get; set; }
+        public int idAnimal { get; set; }
+        public int idMedicamento { get; set; }
+        public DateTime data { get; set; }
     }
 }
