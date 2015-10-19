bb0a133cea78429e89ec6fdfdaeddda48e944de0
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-10-19 21:43:41 -0200

    Medicamento

--- MariosPet/MariosPet/Telas/FrmRemedio.cs
+++ MariosPet/MariosPet/Telas/FrmRemedio.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MariosPet.Classes;
+using MariosPet.Crud;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +14,7 @@ namespace MariosPet.Telas
 {
     public partial class FrmRemedio : Form
     {
+        Medicamento classeMed = new Medicamento();
         public FrmRemedio()
         {
             InitializeComponent();
@@ -32,6 +35,24 @@ namespace MariosPet.Telas
             Telas.FrmMenu menu = new FrmMenu();
             menu.Show();
             Close();
+        }
+
+        public void CopiarParaClasseMedicamento()
+        {
+            //Medicamento
+            classeMed.descricao = txtMedicamento.Text;
+            classeMed.quantidade = Convert.ToInt32(nudQuantidade.Text);
+            classeMed.minimoEmEstoque = Convert.ToInt32(nudMinimo.Text);
+            classeMed.maximoEmEstoque = Convert.ToInt32(nudMaximo.Text);
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            CopiarParaClasseMedicamento();
+
+            CrudMedicamento CrudMed = new CrudMedicamento();
+
+            CrudMed.inserirMedicamento(classeMed);
         }
     }
 }
