1d4d062b6bf9cd86bde48fad612a4600153e66e3
TaiseKeller <taisekf@hotmail.com>
2015-10-19 17:02:25 -0200

    Codigo para validacao

--- MariosPet/MariosPet/Classes/Validacao.cs
+++ MariosPet/MariosPet/Classes/Validacao.cs
@@ -8,5 +8,56 @@ namespace MariosPet.Classes
 {
     class Validacao
     {
+
+        /*   public bool validaAnimal (Animal animal)
+           {
+               bool entrou=true;
+               if (animal.nome.Length < 3)
+               {
+                   MessageBox.Show("Insira o nome do animal!");
+                   entrou= false;
+
+               }                                         
+               if (animal.sexo.Length < 3)
+               {
+                   MessageBox.Show("Insira o sexo do animal!");
+                   entrou = false;
+            }
+           }      
+           public bool validaCliente (Cliente cliente)
+           {
+               bool entrou=true;
+               if (cliente.nome.Length < 3)
+               {
+                   MessageBox.Show("Insira o nome!");
+                   entrou= false;
+
+               }                                         
+               if (cliente.rg.Length < 6)
+               {
+                   MessageBox.Show("Insira o rg!");
+                   entrou = false;
+               }
+              if (cliente.cpf.Length < 6)
+               {
+                   MessageBox.Show("Insira o cpf!");
+                   entrou = false;
+               }
+            }
+         
+            public bool validaEndereco (Endereco endereco)
+           {
+               bool entrou=true;
+               if (endereco.rua.Length < 3)
+               {
+                   MessageBox.Show("Insira o nome da rua!");
+                   entrou= false;
+               }
+               if (endereco.numero <= 0)
+               {
+                  MessageBox.Show("Insira o número!");
+                   entrou= false;
+               }
+      */
     }
 }
