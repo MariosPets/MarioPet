76cec8f03fa9910f6088ed73a7ef8d516208f934
TaiseKeller <taisekf@hotmail.com>
2015-10-19 17:23:08 -0200

    validacao valendo

--- MariosPet/MariosPet/Classes/Validacao.cs
+++ MariosPet/MariosPet/Classes/Validacao.cs
@@ -3,58 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MariosPet.Classes
 {
     class Validacao
     {
 
-        /*   public bool validaAnimal (Animal animal)
-           {
-               bool entrou=true;
-               if (animal.nome.Length < 3)
-               {
-                   MessageBox.Show("Insira o nome do animal!");
-                   entrou= false;
+        public bool validaAnimal(Animal animal)
+        {
+            bool entrou = true;
+            if (animal.nome.Length < 3)
+            {
+                MessageBox.Show("Insira o nome do animal!");
+                entrou = false;
 
-               }                                         
-               
             }
-           }      
-           public bool validaCliente (Cliente cliente)
-           {
-               bool entrou=true;
-               if (cliente.nome.Length < 3)
-               {
-                   MessageBox.Show("Insira o nome!");
-                   entrou= false;
+            return entrou;
 
-               }                                         
-               if (cliente.rg.Length < 6)
-               {
-                   MessageBox.Show("Insira o rg!");
-                   entrou = false;
-               }
-              if (cliente.cpf.Length < 6)
-               {
-                   MessageBox.Show("Insira o cpf!");
-                   entrou = false;
-               }
+        }
+
+        public bool validaCliente(Cliente cliente)
+        {
+            bool entrou = true;
+            if (cliente.nome.Length < 3)
+            {
+                MessageBox.Show("Insira o nome!");
+                entrou = false;
+
             }
-         
-            public bool validaEndereco (Endereco endereco)
-           {
-               bool entrou=true;
-               if (endereco.rua.Length < 3)
-               {
-                   MessageBox.Show("Insira o nome da rua!");
-                   entrou= false;
-               }
-               if (endereco.numero.Length < 1)
-               {
-                  MessageBox.Show("Insira o número!");
-                   entrou= false;
-               }
-      */
+            if (cliente.rg.Length < 6)
+            {
+                MessageBox.Show("Insira o rg!");
+                entrou = false;
+            }
+            if (cliente.cpf.Length < 6)
+            {
+                MessageBox.Show("Insira o cpf!");
+                entrou = false;
+            }
+            return entrou;
+        }
+
+        public bool validaEndereco(Endereco endereco)
+        {
+            bool entrou = true;
+            if (endereco.rua.Length < 3)
+            {
+                MessageBox.Show("Insira o nome da rua!");
+                entrou = false;
+            }
+            if (endereco.numero <= 0) //colocar . Length quando udar numero para string
+            {
+                MessageBox.Show("Insira o número!");
+                entrou = false;
+            }
+            return entrou;
+        }
     }
 }
