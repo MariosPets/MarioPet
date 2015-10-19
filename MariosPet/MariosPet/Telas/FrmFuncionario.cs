072729c2b2c9153aada463d8014eec730f1a37f3
Tsunemoto <cintia.tsunemoto@fatec.sp.gov.br>
2015-10-19 21:34:19 -0200

    Funcionario

--- MariosPet/MariosPet/Telas/FrmFuncionario.cs
+++ MariosPet/MariosPet/Telas/FrmFuncionario.cs
@@ -1,4 +1,5 @@
-﻿using MariosPet.Crud;
+﻿using MariosPet.Classes;
+using MariosPet.Crud;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -74,7 +75,7 @@ namespace MariosPet.Telas
 
             //Endereço
             classeEnd.rua = txtRuaFuncionario.Text;
-            classeEnd.numero = Convert.ToInt32(txtNumeroFuncionario.Text);
+            classeEnd.numero = txtNumeroFuncionario.Text;
             classeEnd.complemento = txtComplementoFuncionario.Text;
             classeEnd.bairro = txtBairroFuncionario.Text;
             classeEnd.cep = Convert.ToInt32(mstCepFuncionario.Text);
