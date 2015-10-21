f133864ae66da217f1bdcce02163f879e8ed2c6f
Plínio <pliniosp@hotmail.com>
2015-10-21 15:17:10 -0200

    correção frmVacina

--- MariosPet/MariosPet/Telas/frmVacina.cs
+++ MariosPet/MariosPet/Telas/frmVacina.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MariosPet.Telas
@@ -17,25 +10,11 @@ namespace MariosPet.Telas
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btmVoltar_Click(object sender, EventArgs e)
         {
-            Telas.FrmFichaClinica fichaclinica = new FrmFichaClinica();
+            FrmFichaClinica fichaclinica = new FrmFichaClinica();
             fichaclinica.Show();
             Close();
-        }
-
-        private void btmLimpar_Click(object sender, EventArgs e)
-        {
-            txtPesquisaVacina.Clear();
-            dtgVacina.ClearSelection();
-            txtVermifugo.Clear();
-            dtgVermifugo.ClearSelection();
-
         }
     }
 }
